48e730a80f687a48b894a4f33a0976f7e0e22418
theFrenchDutch <thomasdeliot@gmail.com>
2015-05-01 11:06:29 +0100

    Added Contrast Shader and Script.

---
+++ Plugin/Shaders/ContrastIntensity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+class ContrastIntensity
+{
+	public float intensity = 0.5f;
+	public Material contrastMaterial;
+
+	void OnRenderImage(RenderTexture source, RenderTexture destination)
+	{
+		contrastMaterial.SetFloat("intensity", intensity);
+		Graphics.Blit(source, destination, contrastMaterial);
+	}
+}
+
