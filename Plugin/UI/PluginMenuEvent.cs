cba9fb2ea20ebd17917d60d21ed60c70c7152fa9
Moon519 <axel.paris69@gmail.com>
2015-05-06 10:10:22 +0100

    Added the pluginMenuEvent class, to deal with the accessibility UI event

---
+++ Plugin/UI/PluginMenuEvent.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PluginMenuEvent : MonoBehaviour 
+{
+	public GameObject[] menuButtons;
+
+	private bool menuEnabled;
+	private bool flagMenuActivate;
+
+	void Start()
+	{
+		menuEnabled = false;
+		flagMenuActivate = false;
+
+		for (int i = 0; i < menuButtons.Length; i++)
+			menuButtons[i].SetActive(false);
+	}
+
+	void Update()
+	{
+		if (menuEnabled && !flagMenuActivate)
+		{
+			flagMenuActivate = true;
+
+			// Show the options of the menu
+			for (int i = 0; i < menuButtons.Length; i++)
+				menuButtons[i].SetActive(true);
+		}
+	}
+
+	public void ActiveAccessibilityMenu()
+	{
+		if (menuEnabled)
+		{
+			menuEnabled = false;
+			flagMenuActivate = false;
+
+			// Desactivate the options of the menu
+			for (int i = 0; i < menuButtons.Length; i++)
+				menuButtons[i].SetActive(false);
+		}
+		else
+			menuEnabled = true;
+	}
+
+	public void ActiveHighContrastMode()
+	{
+
+	}
+
+	public void ActiveColorblindUI()
+	{
+
+	}
+
+	public void RegulateTime()
+	{
+
+	}
+}
