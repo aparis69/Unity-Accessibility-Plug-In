14710f2a166ebee0d984aa5a2c2ababa6efb2d9f
Moon519 <axel.paris69@gmail.com>
2015-05-12 10:45:26 +0100

    Changed player life point magement and game scene

--- Game/Scripts/PlayerLifePoint.cs
+++ Game/Scripts/PlayerLifePoint.cs
@@ -17,6 +17,7 @@ public class PlayerLifePoint : MonoBehaviour
 
 	void OnTriggerEnter(Collider other)
 	{
+		Debug.Log(other.gameObject.tag);
 		// If the player has a contact with a fire, he looses lifepoint
 		if (other.gameObject.CompareTag("Fire"))
 			numberOfPoint -= 1;
