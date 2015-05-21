278cb4232f0bb9886e505e6eca5975c60af5f858
Moon519 <axel.paris69@gmail.com>
2015-05-21 10:19:46 +0100

    Started to create an AudioSourceAccess component

---
+++ Plugin/Scripts/Accessibility Component/AudioSourceAccess.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioSourceAccess : MonoBehaviour 
+{
+	public AudioClip clip;
+	private AudioSource source;
+
+	// State variable
+	private bool isPlaying;
+
+	void Start ()
+	{
+		source = new AudioSource();
+		source.clip = clip;
+		isPlaying = false;
+	}
+
+
+	// Interaction function
+	public void Play()
+	{
+		source.Play();
+		isPlaying = true;
+	}
+
+	public void Stop()
+	{
+		source.Stop();
+		isPlaying = false;
+	}
+
+	public bool IsPlaying()
+	{
+		return isPlaying;
+	}
+}
