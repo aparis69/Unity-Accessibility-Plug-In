881f46ecccc611d012b2dcb59edca733888b2596
theFrenchDutch <thomasdeliot@gmail.com>
2015-05-25 11:59:42 +0100

    Updated subtitle manager, added AudioSourceAccess

--- Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -7,7 +7,7 @@ using Random = UnityEngine.Random;
 namespace UnityStandardAssets.Characters.FirstPerson
 {
     [RequireComponent(typeof (CharacterController))]
-    [RequireComponent(typeof (AudioSource))]
+    [RequireComponent(typeof (AudioSourceAccess))]
     public class FirstPersonController : MonoBehaviour
     {
         [SerializeField] private bool m_IsWalking;
@@ -40,7 +40,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
         private float m_StepCycle;
         private float m_NextStep;
         private bool m_Jumping;
-        private AudioSource m_AudioSource;
+		private AudioSourceAccess m_AudioSourceAccess;
 
 		private bool gameIsOn;
 
@@ -55,7 +55,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
             m_StepCycle = 0f;
             m_NextStep = m_StepCycle/2f;
             m_Jumping = false;
-            m_AudioSource = GetComponent<AudioSource>();
+            m_AudioSourceAccess = GetComponent<AudioSourceAccess>();
 			m_MouseLook.Init(transform , m_Camera.transform);
         }
 
@@ -91,8 +91,8 @@ namespace UnityStandardAssets.Characters.FirstPerson
 
         private void PlayLandingSound()
         {
-            m_AudioSource.clip = m_LandSound;
-            m_AudioSource.Play();
+            m_AudioSourceAccess.source.clip = m_LandSound;
+            m_AudioSourceAccess.Play();
             m_NextStep = m_StepCycle + .5f;
         }
 
@@ -139,8 +139,8 @@ namespace UnityStandardAssets.Characters.FirstPerson
 
         private void PlayJumpSound()
         {
-            m_AudioSource.clip = m_JumpSound;
-            m_AudioSource.Play();
+			m_AudioSourceAccess.source.clip = m_JumpSound;
+            m_AudioSourceAccess.Play();
         }
 
 
@@ -172,11 +172,11 @@ namespace UnityStandardAssets.Characters.FirstPerson
             // pick & play a random footstep sound from the array,
             // excluding sound at index 0
             int n = Random.Range(1, m_FootstepSounds.Length);
-            m_AudioSource.clip = m_FootstepSounds[n];
-            m_AudioSource.PlayOneShot(m_AudioSource.clip);
+			m_AudioSourceAccess.source.clip = m_FootstepSounds[n];
+			m_AudioSourceAccess.PlayOneShot(m_AudioSourceAccess.source.clip);
             // move picked sound to index 0 so it's not picked next time
             m_FootstepSounds[n] = m_FootstepSounds[0];
-            m_FootstepSounds[0] = m_AudioSource.clip;
+			m_FootstepSounds[0] = m_AudioSourceAccess.source.clip;
         }
 
 
